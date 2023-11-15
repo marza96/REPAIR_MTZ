7e5cf8a323f237499a3f11c4c378e4ffbf045592
Haris Sikic <haris.sikic@gmail.com>
2023-11-15 18:37:16 +0100

    Big refactor

--- fuse_same.py
+++ fuse_same.py
@@ -1,12 +1,11 @@
 from net_models.mlp import MLP
+from neural_align import NeuralAlign
 import eval_tools
-from neural_align_diff import NeuralAlignDiff
-
-import argparse
 
 import tqdm
 import copy
 import torch
+import argparse
 import torchvision
 
 import numpy as np
@@ -25,10 +24,10 @@ def load_model(model, i):
     model.load_state_dict(sd)
 
 
-def main(model0_path, model1_path, device="cuda"):
+def main():
     h          = 128
     layers     = 5
-    device     = torch.device(device)
+    device     = torch.device("mps")
 
     transform = transforms.Compose(
         [
@@ -61,6 +60,19 @@ def main(model0_path, model1_path, device="cuda"):
         num_workers=8
     )
 
+    # FashionMnistSet = torchvision.datasets.FashionMNIST(
+    #     root='./data', 
+    #     train=False,
+    #     download=True, 
+    #     transform=transform
+    # )
+    # FashionMnistLoader = torch.utils.data.DataLoader(
+    #     FashionMnistSet, 
+    #     batch_size=128,
+    #     shuffle=True, 
+    #     num_workers=8
+    # )
+
     ConcatTrainLoader = torch.utils.data.DataLoader(
         ConcatDataset((FashionMNISTTrainSet, MNISTTrainSet)), 
         batch_size=128,
@@ -70,42 +82,39 @@ def main(model0_path, model1_path, device="cuda"):
 
     model0 = MLP(h, layers).to(device)
     model1 = MLP(h, layers).to(device)
-    # load_model(model0, "mlps2/fash_mnist_mlp_e50_l5_h128_v2_cuda.pt")
-    # load_model(model1, "mlps2/mnist_mlp_e50_l5_h128_v1_cuda.pt")
-
-    load_model(model0, model0_path)
-    load_model(model1, model1_path)
+    load_model(model0, "./mlps2/fash_mnist_mlp_e50_l5_h128_v1.pt")
+    load_model(model1, "./mlps2/mnist_mlp_e50_l5_h128_v2.pt")
 
     plain_acc               = list()
     permute_acc             = list()
     permute_and_rescale_acc = list()
 
     num_experiments = 10
-    neural_align_ = NeuralAlignDiff(FashionMNISTTrainLoader, MNISTTrainLoader, ConcatTrainLoader)
+    neural_align_ = NeuralAlign()
+    # for i in tqdm.tqdm(range(num_experiments)):
+    #     model0_ = copy.deepcopy(model0)
+    #     model1_ = copy.deepcopy(model1)
+
+    #     modela = neural_align_.fuse_networks(model0_, model1_, i / 10.0, layers, device=device, loader=ConcatTrainLoader, new_stats=False, permute=False).to(device)
+
+    #     plain_acc.append(eval.evaluate_acc(modela, loader=ConcatTrainLoader, device=device))
+    
+    # plt.plot(np.linspace(0, 1.0, num_experiments), plain_acc)
+    # plt.xlabel("alpha")
+    # plt.ylabel("acc")
+    # plt.legend(["plain fusion"])
+    # plt.savefig("./plots/plain.png")
+
+
     for i in tqdm.tqdm(range(num_experiments)):
         model0_ = copy.deepcopy(model0)
         model1_ = copy.deepcopy(model1)
 
-        modela = neural_align_.fuse_networks(model0_, model1_, i / 10.0, layers, device=device, new_stats=False, permute=False).to(device)
+        modela = neural_align_.fuse_networks(model0_, model1_, i / 10.0, layers, device=device, loader=ConcatTrainLoader, new_stats=False, permute=True).to(device)
 
-        plain_acc.append(eval_tools.evaluate_acc(modela, loader=ConcatTrainLoader, device=device))
+        permute_acc.append(eval_tools.evaluate_acc(modela, loader=FashionMNISTTrainLoader, device=device))
     
     plt.figure()
-    plt.plot(np.linspace(0, 1.0, num_experiments), plain_acc)
-    plt.xlabel("alpha")
-    plt.ylabel("acc")
-    plt.legend(["plain fusion"])
-    plt.savefig("./plots/plain.png")
-
-
-    for i in tqdm.tqdm(range(num_experiments)):
-        model0_ = copy.deepcopy(model0)
-        model1_ = copy.deepcopy(model1)
-
-        modela = neural_align_.fuse_networks(model0_, model1_, i / 10.0, layers, device=device, new_stats=False, permute=True).to(device)
-
-        permute_acc.append(eval_tools.evaluate_acc(modela, loader=ConcatTrainLoader, device=device))
-    
     # plt.plot(np.linspace(0, 1.0, num_experiments), plain_acc)
     plt.plot(np.linspace(0, 1.0, num_experiments), permute_acc)
     plt.xlabel("alpha")
@@ -118,18 +127,19 @@ def main(model0_path, model1_path, device="cuda"):
         model0_ = copy.deepcopy(model0)
         model1_ = copy.deepcopy(model1)
 
-        modela = neural_align_.fuse_networks(model0_, model1_, i / 10.0, layers, device=device, new_stats=True, permute=True).to(device)
+        modela = neural_align_.fuse_networks(model0_, model1_, i / 10.0, layers, device=device, loader=ConcatTrainLoader, new_stats=True, permute=True).to(device)
 
-        permute_and_rescale_acc.append(eval_tools.evaluate_acc(modela, loader=ConcatTrainLoader, device=device))
+        permute_and_rescale_acc.append(eval_tools.evaluate_acc(modela, loader=FashionMNISTTrainLoader, device=device))
     
+    plt.figure()
     # plt.plot(np.linspace(0, 1.0, num_experiments), plain_acc)
+    plt.plot(np.linspace(0, 1.0, num_experiments), permute_acc)
     plt.plot(np.linspace(0, 1.0, num_experiments), permute_and_rescale_acc)
     plt.xlabel("alpha")
     plt.ylabel("acc")
     plt.legend(["plain fusion", "permuted fusion", "REPAIR fusion"])
-    plt.savefig("./plots/repair.png")
+    plt.savefig("./plots/permute.png")
    
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--device', default="cuda")
